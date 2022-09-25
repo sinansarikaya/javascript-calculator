6863e61be74c0d380c7c5f8e49a5caeaa23d133a
Sinan <sinan.sarykaya@gmail.com>
2022-09-25 14:23:50 +0200

    Operations added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,41 @@
-const calculatorValue = document.querySelector("#value");
+const storedValue = document.querySelector(".storedValue");
+const calculatorValue = document.querySelector(".value");
 const buttons = document.querySelector(".buttons");
 
 let result = "0";
+let storedNums = null;
+let operator = null;
+let hasSecondVal = false;
+let progress = null;
 
 const showResult = () => {
   calculatorValue.innerText = result;
+  storedValue.innerText = progress;
+  console.log(storedNums);
 };
 showResult();
 
+const calculate = (n1, n2, opt) => {
+  if (opt === "+") {
+    return n1 + n2;
+  } else if (opt === "-") {
+    return n1 - n2;
+  } else if (opt === "*") {
+    return n1 * n2;
+  } else if (opt === "/") {
+    return n1 / n2;
+  }
+
+  return n2;
+};
+
 const numbers = (num) => {
-  result = result === "0" ? num : result + num;
+  if (hasSecondVal) {
+    result = num;
+    hasSecondVal = false;
+  } else {
+    result = result === "0" ? num : result + num;
+  }
 };
 
 const decimal = () => {
@@ -32,12 +58,31 @@ const negativePositive = () => {
 };
 
 const clear = () => {
+  progress = null;
   result = "0";
   showResult();
 };
 
 const operations = (opr) => {
-  console.log(opr);
+  const val = parseFloat(result);
+
+  if (storedNums === null) {
+    storedNums = val;
+  } else if (operator) {
+    const showResult = calculate(storedNums, val, operator);
+
+    result = String(showResult);
+    storedNums = showResult;
+  }
+  hasSecondVal = true;
+  operator = opr;
+
+  if (!val == 0 && opr != "=") {
+    progress === null
+      ? (progress = `${val} ${opr} `)
+      : (progress += `${val} ${opr} `);
+  }
+
   showResult();
 };
 
@@ -50,6 +95,7 @@ buttons.addEventListener("click", (e) => {
 
   if (element.matches(".operator")) {
     operations(element.value);
+    showResult();
     return;
   }
 
